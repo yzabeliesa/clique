237da37a7cfe4385ece84940c776600f889e6f52
lorcanfurey <jericpaul.deleon@gmail.com>
2014-12-10 07:30:08 +0800

    Sanity Check

--- app/views/users/dashboard.blade.php
+++ app/views/users/dashboard.blade.php
@@ -3,5 +3,8 @@
 @section('content')
 <h1>Dashboard</h1>
  
-<p>Welcome to your Dashboard. You rock!</p>
+<p>There once was a maiden from Stonebury Hollow</p>
+<p>She didn't talk much, but boy, did she swallow</p>
+<p>I had a nice lance that she sat upon</p>
+<p>The maiden from Stonebury who is also your mom.</p>
 @stop
